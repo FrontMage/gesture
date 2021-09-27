09a94634234d618112b0dee6f3be259104408d13
FrontMage <xbgxwh@outlook.com>
2021-09-27 17:40:26 +0800

    add gesture start stop

--- zoom.py
+++ zoom.py
@@ -1,3 +1,4 @@
+from typing import Deque
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse, StreamingResponse
 import threading
@@ -7,23 +8,50 @@ from collections import deque
 import asyncio
 import json
 
-zoom_distance = deque(maxlen=30)
+GESTURE_CONTINIUS_TRHESHOLD = 10
+gesture_msgs = deque(maxlen=30)
 frames = deque(maxlen=30)
+gesture_start = deque(maxlen=GESTURE_CONTINIUS_TRHESHOLD)
+
+# Define the gstreamer sink
+gst_str_rtp = 'appsrc ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency byte-stream=true threads=2 key-int-max=15 intra-refresh=true ! h264parse ! rtph264pay ! capssetter caps="application/x-rtp,profile-level-id=(string)42e01f" ! udpsink host=127.0.0.1 port=8004'
+# Cam properties
+fps = 30.0
+frame_width = 640
+frame_height = 480
+
+
+def is_deque_all_true(d: Deque):
+    flag = True
+    for el in d:
+        flag = flag and el
+    return flag
+
+
+def is_deque_all_false(d: Deque):
+    flag = True
+    for el in d:
+        flag = flag and not el
+    return flag
 
 
 def rec_gesture():
     cap = cv2.VideoCapture(1)
-    cap.set(3, 640)
-    cap.set(4, 360)
+    cap.set(3, frame_width)
+    cap.set(4, frame_height)
 
     detector = HandDetector(detectionCon=0.7)
     startDist = None
     scale = 0
+    # Create videowriter as a SHM sink
+    out = cv2.VideoWriter(gst_str_rtp, 0, fps, (frame_width, frame_height), True)
+    is_waiting_gesture_stop = False
     while True:
         success, img = cap.read()
         hands, img = detector.findHands(img)
 
         if len(hands) == 2:
+            gesture_start.append(True)
             if detector.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and detector.fingersUp(
                 hands[1]
             ) == [1, 1, 0, 0, 0]:
@@ -39,17 +67,39 @@ def rec_gesture():
                 )
 
                 scale = int((length - startDist) // 2)
-                zoom_distance.append(scale)
+                gesture_msgs.append(
+                    json.dumps({"gesture_type": "zoom", "scale": scale})
+                )
         else:
+            gesture_start.append(False)
             startDist = None
 
         frames.append(img)
+        if (
+            is_deque_all_true(gesture_start)
+            and len(gesture_start) == GESTURE_CONTINIUS_TRHESHOLD
+            and not is_waiting_gesture_stop
+        ):
+            gesture_msgs.append(json.dumps({"gesture_type": "zoom_start", "scale": 0}))
+            gesture_start.clear()
+            is_waiting_gesture_stop = True
+        elif (
+            is_deque_all_false(gesture_start)
+            and len(gesture_start) == GESTURE_CONTINIUS_TRHESHOLD
+        ):
+            gesture_msgs.append(
+                json.dumps({"gesture_type": "gesture_stop", "scale": 0})
+            )
+            gesture_start.clear()
+            is_waiting_gesture_stop = False
         cv2.imshow("Image", img)
+        # out.write(img)
         if cv2.waitKey(33) == ord("q"):
             break
 
 
 threading.Thread(target=rec_gesture).start()
+# rec_gesture()
 
 app = FastAPI()
 
@@ -99,11 +149,9 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         # data = await websocket.receive_text()
         try:
-            data = zoom_distance.pop()
+            gesture_msg = gesture_msgs.pop()
             try:
-                await websocket.send_text(
-                    json.dumps({"gesture_type": "zoom", "scale": data})
-                )
+                await websocket.send_text(gesture_msg)
             except WebSocketDisconnect:
                 break
         except IndexError:
